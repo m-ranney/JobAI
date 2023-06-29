66d9cc95d7a9e410cbbe86158527f90573157d35
m-ranney <38699117+m-ranney@users.noreply.github.com>
2023-06-29 17:29:20 +0000

    Add URL route on app.py

--- app.py
+++ app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
+# This route is for all URLs submitted by the user for job descriptions
+@app.route('/add_job', methods=['POST'])
+def add_job():
+    url = request.form.get('url')
+    
+    if url is None:
+        return {"error": "No url provided"}, 400
+
+    # Here we will use job_processor to process the URL
+    # job_details = process_url(url)
+
+    # For now, we'll just return a placeholder response
+    return {"message": "Job added successfully", "url": url}
+
+
 if __name__ == '__main__':
     app.run(debug=True)
